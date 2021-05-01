61343e8cf9d92795df8d60ea6c6f6d9ee9e4c514
Samuel Fox <samueljasonfox@gmail.com>
2021-05-01 01:18:42 -0700

    convert SPA to react framework; add styling; update buttons; store image url's in a .json file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
+import Pictures from './utils/images.json'
+import ArrowBackIosOutlinedIcon from '@material-ui/icons/ArrowBackIosOutlined';
+import AutorenewOutlinedIcon from '@material-ui/icons/AutorenewOutlined';
+import HighlightOffOutlinedIcon from '@material-ui/icons/HighlightOffOutlined';
 
 function App() {
+
+  const getRandomNum = () => Math.floor(Math.random() * Pictures.length)
+  const [src, setSrc] = useState(Pictures[getRandomNum()].URL)
+  const [history, setHistory] = useState([])
+  const [timer, setTimer] = useState()
+
+  const getNextPicture = () => {
+    if (timer) return
+    setHistory([...history, src])
+    setSrc(Pictures[getRandomNum()].URL)
+  }
+
+  const handleBack = () => {
+    setSrc(history[history.length - 1])
+    const copy = history
+    copy.pop()
+    setHistory(copy)
+  }
+
+  const startAuto = () => {
+    setTimer(
+      setInterval(() => {
+        setHistory([...history, src])
+        setSrc(Pictures[getRandomNum()].URL)
+      }, 3000)
+    )
+  }
+
+  const stopAuto = () => {
+    clearInterval(timer)
+    setTimer()
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>babe's pups</h1>
+      <div className="flex">
+
+        {history.length > 0 && !timer
+          ? <button className="btn__back" onClick={handleBack}><ArrowBackIosOutlinedIcon /></button>
+          : <button className="btn__back" disabled><ArrowBackIosOutlinedIcon /></button>}
+
+        {history.length === 0 ? <h5> (tap pic or circle to start)</h5> : null}
+
+        {!timer
+          ? <button className="btn__auto__cycle" onClick={startAuto}><AutorenewOutlinedIcon /></button>
+          : <button className="btn__auto__cycle" onClick={stopAuto}><HighlightOffOutlinedIcon /> </button>
+        }
+      </div>
+      <img
+        src={src}
+        width="90%"
+        alt="random doggy"
+        onClick={getNextPicture}
+      />
     </div>
   );
 }
